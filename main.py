42cf75957fee4d6231ceca29606a882a6a13bc5a
Bradley Thomas <bradley.thomas60@googlemail.com>
2022-05-30 18:58:11 +0100

    Building algorithm to remove ".txt" from filenames

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+from array import array
 from fileinput import filename
+from re import L
+from select import select
 from tkinter import *
 
 import os
+from xml.dom.minidom import TypeInfo
 import new_note
 import window
 
@@ -19,24 +23,39 @@ class Main:
         root.geometry("200x270")
         root.grid_columnconfigure(0, weight=1)
         root.resizable(0, 0)
-
+        
         note_list = Frame(root, background="RED")
         note_list.pack(pady=25, side = "top")
         button_frame = Frame(root)
         button_frame.pack(side = "bottom")
 
+        def open_note(event):
+            _selection = get_selection()
+            selection = list(_selection)
+            selection_delete_point = selection.__len__() - 4
+           
+            counter = 0
+            
+            # selection is now an array
+            # - loop throguh array to find ".txt" to os.remove
+            # - remove them from array
+            # - convert back to string again
+            
+            
+        def get_selection(): 
+            selection = notes_list.get(ANCHOR)
+            return selection
+            
         notes_list = Listbox(note_list, width=40, height=9)
         notes_list.pack()
+        notes_list.bind("<Button-1>", open_note) # OPEN FROM CLASS
         
-        # listbox_selection = note_list.get()
-        # print(listbox_selection)
+
 
         for notes in file_struct:
             notes_list.insert(END, notes)
             
-       
 
-        # notes_list.bind("<Button-1>", open_note) # OPEN FROM CLASS
         
         open_button = Button(button_frame, text="New Note", command=new_note.Note)
         open_button.pack(pady=10)
--- new_note.py
+++ new_note.py
@@ -25,14 +25,18 @@ class Note:
         Label(self.new_note_frame, text = "Enter a new name for your note:").pack()
         self.name_input = Entry(self.new_note_frame).pack(pady = 5)
         self.new_name_input = self.name_input#.get()
+     
+        def submit(event):
+            # ########## take note of text input for selection later ##########
+            print(self.name_input.get())
+            self.new_note.destroy()
         
         self.submit_button = Button(self.new_note_frame, text = "Submit")
         self.submit_button.pack()
-        self.submit_button.bind("<Button-1>", self.new_note.mainloop())
+        self.submit_button.bind("<Button-1>", submit)
 
 
-
-
+        self.new_note.mainloop()
         
         # TKINTER WINDOW ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
